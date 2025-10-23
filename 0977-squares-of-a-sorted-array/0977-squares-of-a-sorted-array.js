0a12b2765e34fd57031029651d48950fc2a59d22
ansifmk <mohammedansifmka@gmail.com>
2025-10-23 09:47:57 +0530

    Time: 8 ms (53.89%), Space: 61.2 MB (84.25%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let squared=nums.map((num)=>num*num)
+     squared.sort((a,b)=>a-b)
+     return squared
+};
