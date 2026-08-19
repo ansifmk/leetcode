4e924c0a289684f946eed22492d5c5a4c8beee2b
ansif mk <mohammedansifmka@gmail.com>
2026-08-19 09:53:44 +0530

    Time: 691 ms (9.98%), Space: 0B (100%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select S.student_id,S.student_name,SU.subject_name,count (E.student_id) attended_exams
+from Students S
+cross join Subjects SU
+left join Examinations E
+on S.student_id=E.student_id and SU.subject_name=E.subject_name
+group by S.student_id, S.student_name,SU.subject_name
+order by S.student_id, S.student_name,SU.subject_name
